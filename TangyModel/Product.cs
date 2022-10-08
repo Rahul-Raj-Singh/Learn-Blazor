1e438b97402400608c19399921b4da45438c0f9f
Rahul Singh <rahul.r.singh@contractor.p66.com>
2022-10-08 18:43:09 +0530

    Added Blazored.Modal example

--- TangyModel/Product.cs
+++ TangyModel/Product.cs
@@ -1,7 +1,10 @@
-﻿namespace TangyModel;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TangyModel;
 public class Product
 {
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
     public decimal Price { get; set; }
     public bool IsActive { get; set; }
